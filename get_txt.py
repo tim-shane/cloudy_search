ecee2a961b35c450e2ebd6193c87430962cf7f25
Tim Edwards <timedwards8@gmail.com>
2017-06-16 13:09:13 -0500

    Update get_txt.py

--- get_txt.py
+++ get_txt.py
@@ -1,10 +1,10 @@
 from py_ms_cognitive import PyMsCognitiveWebSearch
 
-api = 'API_KEY'
+
 # Changing to v7 so you only need one API key.
 PyMsCognitiveWebSearch.SEARCH_WEB_BASE = 'https://api.cognitive.microsoft.com/bing/v7.0/search'
 
-def return_txt(search_term):
+def return_txt(api, search_term):
     txt = ""
     bing_txt = PyMsCognitiveWebSearch(api, search_term)
     first_result = bing_txt.search()
