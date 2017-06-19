4eae23504b5dfa96cd8c014151aeb38a53394b4d
Romain Richard <romain.richard.IT.engineer@gmail.com>
2017-06-19 01:06:27 -0700

    Simplify the construction of the txt variable
    You should double check, couldn't test it myself because I have no API key. But it should return the same result as before, just a bit more readable now.

--- get_txt.py
+++ get_txt.py
@@ -5,11 +5,7 @@ from py_ms_cognitive import PyMsCognitiveWebSearch
 PyMsCognitiveWebSearch.SEARCH_WEB_BASE = 'https://api.cognitive.microsoft.com/bing/v7.0/search'
 
 def return_txt(api, search_term):
-    txt = ""
     bing_txt = PyMsCognitiveWebSearch(api, search_term)
     first_result = bing_txt.search()
-    snippets = []
-    snippets.append([snip.snippet for snip in first_result])
-    for word in snippets:
-        txt += str(word)
+    txt = str([snip.snippet for snip in first_result]])
     return txt
